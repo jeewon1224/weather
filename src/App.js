90584673baafbff676930c4106a33d6945e69140
jeewon94 <jayyy1224@gmail.com>
2021-04-08 17:57:28 +0900

    04/08 (목) 리액트 날씨 api 활용 앱 만들기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Axios from 'axios';
+import DisplayData from './components/DisplayData';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+
+  state ={
+    coords :{
+      latitude : 37,
+      longitude :127
+    },
+    data : {},
+    inputData : ""
+  }
+
+  componentDidMount(){
+    // console.log('render mounted');
+    if(navigator.geolocation){
+      // console.log('supported');
+      navigator.geolocation.getCurrentPosition((position) => {
+        // console.log(position.coords.latitude, position.coords.longitude);
+      
+      let newCoords ={
+        latitude : position.coords.latitude,
+        longitude : position.coords.longitude
+      }
+      this.setState({coords : newCoords});
+      
+      Axios.get(`http://api.weatherstack.com/current?access_key=698470f5e00905b485d845b29a1f854d&query=${this.state.coords.latitude},${this.state.coords.longitude}`).then((res)=> {
+          // console.log(res);
+          let weatherData = {
+            temperature: res.data.current.temperature,
+            description: res.data.current.weather_descriptions[0],
+            location: res.data.location.name,
+            region: res.data.location.region,
+            country: res.data.location.country,
+            wind_speed: res.data.current.wind_speed,
+            pressure: res.data.current.pressure,
+            precip: res.data.current.precip,
+            humidity: res.data.current.humidity,
+            img: res.data.current.weather_icons
+
+          }
+          this.setState({data : weatherData});
+        })
+
+
+
+      });
+    }else{
+      console.log('not supported');
+    }
+  }
+
+  change = (value) => {
+    // console.log(value);
+    this.setState({inputData : value});
+    // console.log(this.state.inputData);
+  }
+
+  changeWeather = (event) =>{
+    event.preventDefault();
+    Axios.get(`http://api.weatherstack.com/current?access_key=698470f5e00905b485d845b29a1f854d&query=${this.state.inputData}`).then((res)=> {
+          // console.log(res);
+          let weatherData = {
+            temperature: res.data.current.temperature,
+            description: res.data.current.weather_descriptions[0],
+            location: res.data.location.name,
+            region: res.data.location.region,
+            country: res.data.location.country,
+            wind_speed: res.data.current.wind_speed,
+            pressure: res.data.current.pressure,
+            precip: res.data.current.precip,
+            humidity: res.data.current.humidity,
+            img: res.data.current.weather_icons
+
+          }
+          this.setState({data : weatherData});
+        })
+
+  }
+
+
+
+
+  render(){
+
+    return (
+      <div className="App">
+        <div className="container">
+          <Navbar changeRegion ={this.change} changeWeather = {this.changeWeather} />
+          <DisplayData weather = {this.state.data} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Navbar(props){
+  return (
+    <div className="row">
+      <div className="col-md-6">
+        <h1 className="title">Realtime Weather</h1>
+      </div>
+      <div className="col-md-6">
+        <form className="region" onSubmit = {(e) => props.changeWeather(e)} >
+          <input className="regioninput" placeholder="Enter Your Location"  onChange={(e) => {props.changeRegion(e.target.value)}}/>
+        </form>    
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
